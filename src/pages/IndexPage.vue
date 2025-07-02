58da9e30eba982b7aecba225b040d7effbd7f83e
supissara-372 <supissaramukwin@gmail.com>
2025-07-02 11:05:40 +0700

    add new image information

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -5,6 +5,11 @@
       src="~assets/quasar-logo-vertical.svg"
       style="width: 200px; height: 200px"
     >
+        <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    >
   </q-page>
 </template>
 
